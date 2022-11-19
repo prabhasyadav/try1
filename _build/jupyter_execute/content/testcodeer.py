78df3a4a7c5229da902f8f942894c4c76746ddc9
vibhubatheja <vibhubatheja2@gmail.com>
2022-11-19 12:35:20 +0100

    v0.01.12

--- _build/jupyter_execute/content/testcodeer.py
+++ _build/jupyter_execute/content/testcodeer.py
@@ -16,37 +16,50 @@
 # <script src="https://unpkg.com/thebe@latest/lib/index.js"></script>
 # 
 
+# <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" />
 # <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"></script>
 # 
 # <script type="text/x-thebe-config">
 #   {
 #     requestKernel: true,
 #     binderOptions: {
-#       repo: "plotly/plotly.py",
-#       ref: "doc-prod",
-#       binderUrl: "https://mybinder.org",
+#       repo: "matplotlib/ipympl",
+#       ref: "0.6.1",
 #       repoProvider: "github",
 #     },
 #   }
 # </script>
 # <script src="https://unpkg.com/thebe@latest/lib/index.js"></script>
 # 
-# <button id="activateButton" style="width: 120px; height: 40px; font-size: 1.5em;">
-#   Activate
+# <button id="activateButton" style="width: 120px; height: 45px; font-size: 1.5em;">
+#      Activate
 # </button>
 # <script>
-# var bootstrapThebe = function() {
-#     thebe.bootstrap();
-# }
-# document.querySelector("#activateButton").addEventListener('click', bootstrapThebe)
+#    var bootstrapThebe = function() {
+#        thebe.bootstrap();
+#    }
+#    document.querySelector("#activateButton").addEventListener('click', bootstrapThebe)
 # </script>
 # 
-# <pre data-executable="true" data-language="python">
-# import plotly.express as px
-# df = px.data.tips()
-# fig = px.histogram(df, x="total_bill", y="tip", color="sex", marginal="rug",
-#                    hover_data=df.columns)
-# fig.show()
-# </pre>
+# 
+# 
+#    <pre data-executable="true" data-language="python">
+#    %matplotlib widget
+#    import ipywidgets as widgets
+#    import matplotlib.pyplot as plt
+#    import numpy as np
+# 
+#    x = np.linspace(0,10)
+# 
+#    def sine_func(x, w, amp):
+#        return amp*np.sin(w*x)
+# 
+#    @widgets.interact(w=(0, 4, 0.25), amp=(0, 4, .1))
+#    def update(w = 1, amp = 1):
+#        plt.clf()
+#        plt.ylim(-4, 4)
+#        plt.plot(x, sine_func(x, w, amp))
+#    </pre>
+# 
 
 # 
